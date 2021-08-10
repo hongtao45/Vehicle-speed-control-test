a4b1280ad7643df1e727f1adf8a2415a74ae1950
hongtao45 <42789014+hongtao45@users.noreply.github.com>
2021-08-10 21:49:22 +0800

    添加了两个方案之间的对比效果

--- test.py
+++ test.py
@@ -10,25 +10,24 @@
 @版本   :v1.0
 '''
 
+
 import traci
 import sumolib
 from tqdm import tqdm
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
-def main_test(use_gui= False):
+def main_test(car_speed, road_speed, use_gui= False):
+
     if use_gui:
         sb = sumolib.checkBinary('sumo-gui')
     else:
         sb = sumolib.checkBinary('sumo')
 
-
     traci.start([sb,
                 "-n", "test.net.xml",
                 "-r", "test.rou.xml",
                 "-b", "0",
-                "-e", "200",
                 "--no-warnings", "true",
                 "--delay","100",
                 "--start", "true",
@@ -37,44 +36,58 @@ def main_test(use_gui= False):
     
     edge_list = ['gneE1', 'gneE2', 'gneE3', 'gneE4'] # 只有两个路段
     
+
     ## 1 道路的限速
-    maxSp =[5, 7, 9, 11]
     for x in range(len(edge_list)):
-        traci.edge.setMaxSpeed(edge_list[x], maxSp[x])
+        traci.edge.setMaxSpeed(edge_list[x], road_speed[x])
     
-    ## 2 车自身的限速
-    traci.vehicle.setMaxSpeed('vehicle_0', 8)
+
+    ## 2 考虑 车自带的限速  
+    if car_speed > 0: # # car_speed = 0 # 表示不给车限速
+        traci.vehicle.setMaxSpeed('vehicle_0', car_speed)
+
 
     ## 3 收集参数
     sp_list = []
-    sp2_list = []
 
-    
     pbar = tqdm(range(200))
-    for x in pbar:
-        sp = traci.vehicle.getSpeed('vehicle_0')
-        sp2 = traci.vehicle.getSpeedWithoutTraCI('vehicle_0') # 欸有traci影响时的速度
+    while traci.simulation.getMinExpectedNumber() > 0:
+        sp = traci.vehicle.getSpeed('vehicle_0') # 没有车的时候就会报错了
 
         sp_list.append(sp)
-        sp2_list.append(sp2)
 
-        
         t = traci.simulation.getTime()
         traci.simulationStep()  # 仿真跑一秒
         pbar.set_description(f"Processing: {t} ")
+        pbar.update(1)
 
     traci.close()
-
-    da = pd.DataFrame(sp_list)
-    da['sp2'] = sp2_list
-
-    da.columns =['sp', 'sp2']
-    da.to_csv('speed.csv', index= None)
+    
+    return sp_list # 返回列表
 
 
 if __name__ == "__main__":
 
     use_gui = False
     # use_gui = True
-    main_test(use_gui)
+
+    
+    ##  车 有 限速值
+    car_speed = 18
+    road_speed = range(15, 19) 
+    sp_y = main_test(car_speed, road_speed, use_gui)  
+    
+    ##  车 没有 限速值
+    car_speed = 11 
+    road_speed = range(11,15)
+    sp_n = main_test(car_speed, road_speed, use_gui) # car_speed <= 0 # 表示不给车限速 
+
+    min_len = min(len(sp_y), len(sp_n))
+
+
+    da = pd.DataFrame()
+    da["with"] = sp_y[0: min_len] # 保证长度一样
+    da["without"] = sp_n[0: min_len] 
+
+    da.to_csv('speed.csv', index= None)
 
